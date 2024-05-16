6e2ac04a992605fe598c1fa411edd1cc64243a27
faysal <faysal.hasan@welldev.io>
2024-05-16 10:18:29 +0600

    project created

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const originalUrl = 'http://localhost:8080';
+
+export const environment = {
+  production: true,
+  backendUrl: `${originalUrl}/trueAct/`,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const originalUrl = 'http://localhost:8080';
+
+export const environment = {
+  production: false,
+  backendUrl: `${originalUrl}/trueAct/`,
+};
+
